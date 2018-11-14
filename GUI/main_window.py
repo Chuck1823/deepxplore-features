0677f8b600694e811075d90092c7215630860a61
Charles Antoine Malenfant <charles_m.18@hotmail.com>
2018-11-14 13:50:53 -0500

    Fix made to prevent user from editing dropdown menu.

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -5,7 +5,7 @@ layout = [[sg.Text('Activation treshold'), sg.Input()],
           [sg.Text('Number of seeds'), sg.Input()],
           [sg.Text('Step size'), sg.Input()],
           [sg.Text('Iterations'), sg.Input()],
-          [sg.Text('Gradient ascent'), sg.Input(),sg.Text('Testing database'),sg.InputCombo(('Drebin', 'Driving', 'ImageNet', 'MNIST', 'PDF'), size=(20, 1))],
+          [sg.Text('Gradient ascent'), sg.Input(),sg.Text('Testing database'),sg.InputCombo(('Drebin', 'Driving', 'ImageNet', 'MNIST', 'PDF'), size=(20, 1), readonly=True)],
           [sg.Submit()]]
 
 event, (aTresh,behavDiff,numSeeds,stepSize,iterations,gradAscent,testingDB) = sg.Window('DeepXplore GUI').Layout(layout).Read()
