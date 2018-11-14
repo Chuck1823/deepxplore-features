09626a635c089690889d9db79d05a27deb11eb1d
Charles Antoine Malenfant <charles_m.18@hotmail.com>
2018-11-13 23:56:24 -0500

    Added dropdown menu to select dataset to be tested, still need to fix some bug where user can enter its own DB..

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -5,9 +5,9 @@ layout = [[sg.Text('Activation treshold'), sg.Input()],
           [sg.Text('Number of seeds'), sg.Input()],
           [sg.Text('Step size'), sg.Input()],
           [sg.Text('Iterations'), sg.Input()],
-          [sg.Text('Gradient ascent'), sg.Input()],
+          [sg.Text('Gradient ascent'), sg.Input(),sg.Text('Testing database'),sg.InputCombo(('Drebin', 'Driving', 'ImageNet', 'MNIST', 'PDF'), size=(20, 1))],
           [sg.Submit()]]
 
-event, (aTresh,behavDiff,numSeeds,stepSize,iterations,gradAscent,) = sg.Window('DeepXplore GUI').Layout(layout).Read()
+event, (aTresh,behavDiff,numSeeds,stepSize,iterations,gradAscent,testingDB) = sg.Window('DeepXplore GUI').Layout(layout).Read()
 
-sg.Popup(event, aTresh, behavDiff, numSeeds, stepSize, iterations, gradAscent)
+sg.Popup(event, aTresh, behavDiff, numSeeds, stepSize, iterations, gradAscent, testingDB)
